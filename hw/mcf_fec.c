rzr/qemu

commit 0c6b93c267dd71776ca2acd5040ce1859a8183e8

add missing MAC address to info_str for some NICs (Mark McLoughlin)
Signed-off-by: Mark McLoughlin <markmc@redhat.com>
Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>



git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@6219 c046a42c-6fe2-441c-8c8c-71466251a162

hw/mcf_fec.c
@@ -455,4 +455,5 @@ void mcf_fec_init(NICInfo *nd, target_phys_addr_t base, qemu_irq *irq)
     s->vc = qemu_new_vlan_client(nd->vlan, nd->model,
                                  mcf_fec_receive, mcf_fec_can_receive, s);
     memcpy(s->macaddr, nd->macaddr, 6);
+    qemu_format_nic_info_str(n->vc, s->macaddr);
 }

hw/smc91c111.c
@@ -706,5 +706,6 @@ void smc91c111_init(NICInfo *nd, uint32_t base, qemu_irq irq)
 
     s->vc = qemu_new_vlan_client(nd->vlan, nd->model,
                                  smc91c111_receive, smc91c111_can_receive, s);
+    qemu_format_nic_info_str(s->vc, s->macaddr);
     /* ??? Save/restore.  */
 }

hw/stellaris_enet.c
@@ -396,9 +396,11 @@ void stellaris_enet_init(NICInfo *nd, uint32_t base, qemu_irq irq)
     s->irq = irq;
     memcpy(s->macaddr, nd->macaddr, 6);
 
-    if (nd->vlan)
+    if (nd->vlan) {
         s->vc = qemu_new_vlan_client(nd->vlan, nd->model,
                                      stellaris_enet_receive, stellaris_enet_can_receive, s);
+        qemu_format_nic_info_str(s->vc, s->macaddr);
+    }
 
     stellaris_enet_reset(s);
     register_savevm("stellaris_enet", -1, 1,

hw/virtio-net.c
@@ -318,6 +318,8 @@ PCIDevice *virtio_net_init(PCIBus *bus, NICInfo *nd, int devfn)
     n->vc = qemu_new_vlan_client(nd->vlan, nd->model,
                                  virtio_net_receive, virtio_net_can_receive, n);
 
+    qemu_format_nic_info_str(n->vc, n->mac);
+
     n->tx_timer = qemu_new_timer(vm_clock, virtio_net_tx_timer, n);
     n->tx_timer_active = 0;
     n->mergeable_rx_bufs = 0;

commit 1fa7ac84f352b77c46b8396234942a33b7d97f43

Fix build breakage introduced by r6219
This was spotted by Stefan Weil.

Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>



git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@6224 c046a42c-6fe2-441c-8c8c-71466251a162

hw/mcf_fec.c
@@ -455,5 +455,5 @@ void mcf_fec_init(NICInfo *nd, target_phys_addr_t base, qemu_irq *irq)
     s->vc = qemu_new_vlan_client(nd->vlan, nd->model, nd->name,
                                  mcf_fec_receive, mcf_fec_can_receive, s);
     memcpy(s->macaddr, nd->macaddr, 6);
-    qemu_format_nic_info_str(n->vc, s->macaddr);
+    qemu_format_nic_info_str(s->vc, s->macaddr);
 }