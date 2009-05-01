ceph/qemu-kvm

Build fix for !CONFIG_GDBSTUB case
Once CONFIG_GDBSTUB not configured, compile will generate error.

Signed-off-by: Xiantao Zhang <xiantao.zhang@intel.com>
Signed-off-by: Avi Kivity <avi@redhat.com>

vl.c
@@ -3862,10 +3862,12 @@ static int main_loop(void)
 		qemu_system_powerdown();
                 ret = EXCP_INTERRUPT;
             }
+#ifdef CONFIG_GDBSTUB
             if (unlikely(ret == EXCP_DEBUG)) {
                 gdb_set_stop_cpu(cur_cpu);
                 vm_stop(EXCP_DEBUG);
             }
+#endif
             /* If all cpus are halted then wait until the next IRQ */
             /* XXX: use timeout computed from timers */
             if (ret == EXCP_HALTED) {

rzr/qemu

Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

vl.c
@@ -4350,11 +4350,13 @@ static void tcg_cpu_exec(void)
         }
         if (cpu_can_run(env))
             ret = qemu_cpu_exec(env);
+#ifndef CONFIG_GDBSTUB
         if (ret == EXCP_DEBUG) {
             gdb_set_stop_cpu(env);
             debug_requested = 1;
             break;
         }
+#endif
     }
 }
 