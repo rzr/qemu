4fe51ce326bd654d812219c3999bd87224527d69
munkyu.im <munkyu.im@samsung.com>
2011-05-16 13:31:20 +0900

    add qemu_configure.sh (It enables to add option to the configure file)

---
+++ qemu_configure.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# OS specific
+#--target-list=i386-softmmu,arm-softmmu \
+targetos=`uname -s`
+case $targetos in
+Linux*)
+echo "checking for os... targetos $targetos"
+exec ./configure \
+ --target-list=i386-softmmu \
+ --enable-profiler \
+ --disable-werror \
+ --audio-drv-list=alsa \
+ --audio-card-list=es1370 \
+ --enable-mixemu \
+ --disable-vnc-tls \
+# --enable-gles2 --gles2dir=/usr
+;;
+CYGWIN*)
+echo "checking for os... targetos $targetos"
+exec ./configure \
+ --target-list=i386-softmmu \
+ --audio-drv-list=dsound \
+ --audio-card-list=es1370 \
+ --enable-mixemu \
+ --disable-vnc-tls 
+;;
+MINGW*)
+echo "checking for os... targetos $targetos"
+exec ./configure \
+ --target-list=i386-softmmu,arm-softmmu \
+ --audio-drv-list=dsound \
+ --audio-card-list=es1370 \
+ --enable-mixemu \
+ --disable-vnc-tls \
+ --disable-vnc-jpeg \
+ --disable-jpeg
+;;
+esac
