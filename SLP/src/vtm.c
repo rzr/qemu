f00bccfdf3d13d45b3471175ab9ae081ec999c46
munkyu.im <munkyu.im@samsung.com>
2011-11-22 17:06:18 +0900

    [Title]    delete ssh redir port [Type]      // Commit Type: Feature / Bugfix / Enhancement [Module]    // Module Name - (Main / Sub) [Priority] [CQ#] [Redmine#] [Problem] [Cause] [Solution] [TestCase]

--- SLP/src/vtm.c
+++ SLP/src/vtm.c
@@ -667,7 +667,7 @@ int create_config_file(gchar* filepath)
 		
 		g_fprintf (fp, "\n[%s]\n", ADDITIONAL_OPTION_GROUP);
 		g_fprintf (fp, "%s=\n", EMULATOR_OPTION_KEY);
-		g_fprintf (fp, "%s=%s\n", QEMU_OPTION_KEY,"-usb -usbdevice wacom-tablet -usbdevice keyboard -net nic,model=virtio -soundhw all -rtc base=utc -net user -redir tcp:1202:10.0.2.16:22");
+		g_fprintf (fp, "%s=%s\n", QEMU_OPTION_KEY,"-usb -usbdevice wacom-tablet -usbdevice keyboard -net nic,model=virtio -soundhw all -rtc base=utc -net user");
 		g_fprintf (fp, "[%s]\n", HARDWARE_GROUP);
 		g_fprintf (fp, "%s=\n", RESOLUTION_KEY);
 		g_fprintf (fp, "%s=1\n", BUTTON_TYPE_KEY);
