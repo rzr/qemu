059ef42fb1ff543fd147368f52b8159b443b936f
munkyu.im <munkyu.im@samsung.com>
2011-11-26 15:37:46 +0900

    [Title]    remove fprintf(...) [Type]      // Commit Type: Feature / Bugfix / Enhancement [Module]    // Module Name - (Main / Sub) [Priority] [CQ#] [Redmine#] [Problem] [Cause] [Solution] [TestCase]

--- tizen/src/debug_ch_vtm.c
+++ tizen/src/debug_ch_vtm.c
@@ -270,10 +270,7 @@ static void debug_init(void)
 	strcat(logfile, "/vtm.log");
 
 	if(access(logfile, F_OK | R_OK) == 0)
-	{
-		if(remove(logfile) == 0)
-			fprintf(stdout, "origianl (%s) is removed\n", logfile);
-	}
+		remove(logfile) == 0
 }
 
 /* allocate some tmp string space */
