104ad8e8cd79c5d6ef42def178da1f847727dd20
munkyu.im <munkyu.im@samsung.com>
2011-12-01 22:25:19 +0900

    [Title] Remove Shell option on menu [Type] Enhancement [Module] [Priority] [CQ#] [Redmine#] [Problem] [Cause] [Solution] [TestCase]

--- tizen/src/menu_callback.c
+++ tizen/src/menu_callback.c
@@ -645,13 +645,13 @@ void show_info_window(GtkWidget *widget, gpointer data)
 #ifndef _WIN32		
 	details = g_strdup_printf("Name: %s\nCPU: %s\nResolution: %s\nButton type: %d button(s)\nRam size: %s\nDPI: %s\nSD card: %s\nSD path: %s\nDisk path: %s",
 			target_name, arch, resolution, button_type, ram_size_detail, dpi, sdcard_detail, sdcard_path_detail, disk_path_detail);
-	show_message("Emulator details", details);
+	show_message("Emulator Details", details);
 #else /* _WIN32 */
 	gchar *details_win = NULL;
 	details = g_strdup_printf("Name: %s\nCPU: %s\nResolution: %s\nButton type: %d button(s)\nRam size: %s\nDPI: %s\nSD card: %s\nSD path: %s\nDisk path: %s",
 			target_name, arch, resolution, button_type, ram_size_detail, dpi, sdcard_detail, sdcard_path_detail, disk_path_detail);
 	details_win = change_path_from_slash(details);
-	show_message("Emulator details", details_win);
+	show_message("Emulator Details", details_win);
 	free(details_win);
 #endif
 	g_free(resolution);
