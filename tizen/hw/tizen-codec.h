ae67ef1f212269715d2f3d57409ef2295c9200a6
kt920.kim <kt920.kim@samsung.com>
2011-12-15 20:30:15 +0900

    [Title] Add boilterplate template for Codec Module [Type] [Module] Codec [Priority] [CQ#] [Redmine#] [Problem] [Cause] [Solution] [TestCase]

--- tizen/hw/tizen-codec.h
+++ tizen/hw/tizen-codec.h
@@ -1,3 +1,33 @@
+/*
+ * Virtual Codec device(PCI)
+ *
+ * Copyright (c) 2011 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Authors:
+ *  Kitae Kim <kt920.kim@samsung.com>
+ *  SeokYeon Hwang <syeon.hwang@samsung.com>
+ *  DongKyun Yun <dk77.yun@samsung.com>
+ *  YeongKyoon Lee <yeongkyoon.lee@samsung.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Contributors:
+ * - S-Core Co., Ltd
+ *
+ */
+
 #include <stdio.h>
 #include <pthread.h>
 #include <sys/types.h>
@@ -10,6 +40,7 @@
 #include <libswscale/swscale.h>
 
 // #define CODEC_THREAD
+
 /*
  *  Codec Device Structures
  */
