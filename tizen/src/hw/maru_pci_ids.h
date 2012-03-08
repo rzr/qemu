f341b2132b355095678ffa8f1f9361483cea4699
Son Hyunjun <hj79.son@samsung.com>
2012-03-08 14:16:55 +0900

    change boilerplate

--- tizen/src/hw/maru_pci_ids.h
+++ tizen/src/hw/maru_pci_ids.h
@@ -1,7 +1,7 @@
 /*
  * 
  *
- * Copyright (C) 2000 - 2012 Samsung Electronics Co., Ltd. All rights reserved.
+ * Copyright (C) 2011 - 2012 Samsung Electronics Co., Ltd. All rights reserved.
  *
  * Contact:
  * DoHyung Hong <don.hong@samsung.com>
