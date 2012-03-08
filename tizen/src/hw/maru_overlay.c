ffd95f2e0f8708a3465fb6b29b1c89205e23ddfb
Son Hyunjun <hj79.son@samsung.com>
2012-03-08 14:00:14 +0900

    modify boilerplate and add include <pthread.h>

--- tizen/src/hw/maru_overlay.c
+++ tizen/src/hw/maru_overlay.c
@@ -1,7 +1,7 @@
 /* 
  * Qemu overlay emulator for VGA
  *
- * Copyright (C) 2000 - 2011 Samsung Electronics Co., Ltd. All rights reserved.
+ * Copyright (C) 2011 - 2012 Samsung Electronics Co., Ltd. All rights reserved.
  *
  * Contact: 
  * DoHyung Hong <don.hong@samsung.com>
--- tizen/src/skin/skin_server.c
+++ tizen/src/skin/skin_server.c
@@ -1,7 +1,7 @@
 /*
  * socket server for emulator skin
  *
- * Copyright (C) 2000 - 2012 Samsung Electronics Co., Ltd. All rights reserved.
+ * Copyright (C) 2011 - 2012 Samsung Electronics Co., Ltd. All rights reserved.
  *
  * Contact:
  * Hyunjun Son <hj79.son@samsung.com>
