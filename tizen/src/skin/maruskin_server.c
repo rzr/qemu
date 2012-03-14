75cfafc863c1482f9d0521117ffa1b8c0d24a97d
Son Hyunjun <hj79.son@samsung.com>
2012-03-15 01:41:18 +0900

    use qemu thread

--- tizen/src/skin/maruskin_server.c
+++ tizen/src/skin/maruskin_server.c
@@ -47,6 +47,7 @@
 #include "maruskin_operation.h"
 #include "debug_ch.h"
 #include "bswap.h"
+#include "qemu-thread.h"
 
 MULTI_DEBUG_CHANNEL( qemu, maruskin_server );
 
@@ -101,9 +102,9 @@ pthread_t start_skin_server( uint16_t default_svr_port, int argc, char** argv ) 
 
     pthread_t thread_id;
 
-    if ( 0 != pthread_create( &thread_id, NULL, run_skin_server, NULL ) ) {
-        ERR( "fail to create skin_server pthread.\n" );
-    }
+    QemuThread thread;
+
+    qemu_thread_create( &thread, run_skin_server, NULL );
 
     return thread_id;
 
