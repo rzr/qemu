cf7f791e9d0eb7f9cf4002f6b0d5c2338a9434b0
syeon.hwang <syeon.hwang@samsung.com>
2012-03-15 13:20:35 +0900

    [Title] Remove all items for clean upgrade 1.0.1 base code [Type] [Module] Emulator/ [Priority] [CQ#] [Redmine#] [Problem] [Cause] [Solution] [TestCase]

--- tizen/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#Build-Depends: bison, flex, autoconf, gcc, libglu1-mesa-dev, libsdl1.2-dev, libgtk2.0-dev, libsdl-image1.2-dev, libsdl-gfx1.2-dev, debhelper, libxml2-dev, libasound2-dev
-
-autoconf
-./configure
-make
