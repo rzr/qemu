7bde86a4dc41cadd1c9c380af6c9088988b83d64
Son Hyunjun <hj79.son@samsung.com>
2012-03-23 15:24:57 +0900

    [Title] fix compile error [Type] Bugfix [Module] Skin [Priority] Minor [CQ#] [Redmine#] [Problem] [Cause] [Solution]

--- tizen/src/skin/client/src/org/tizen/emulator/skin/screenshot/ScreenShotDialog.java
+++ tizen/src/skin/client/src/org/tizen/emulator/skin/screenshot/ScreenShotDialog.java
@@ -33,102 +33,138 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.graphics.ImageLoader;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Dialog;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.ToolBar;
-import org.eclipse.swt.widgets.ToolItem;
+import org.tizen.emulator.skin.config.EmulatorConfig;
+import org.tizen.emulator.skin.log.SkinLogger;
 import org.tizen.emulator.skin.util.IOUtil;
+import org.tizen.emulator.skin.util.SkinUtil;
 
-public class ScreenShotDialog {
+public class ScreenShotDialog extends Dialog {
+
+	private Logger logger = SkinLogger.getSkinLogger( ScreenShotDialog.class ).getLogger();
+
 	private Image image;
-	private Shell dialog;
 	private Shell shell;
-
+	private Shell parent;
+	private EmulatorConfig config;
+	
 	private Canvas canvas;
-	private ToolBar bar ;
 
-	public ScreenShotDialog( Shell shell, Canvas lcdCanvas ) {
-		this.shell = shell;
+	public ScreenShotDialog( Shell parent, Canvas lcdCanvas, EmulatorConfig config ) {
+		super( parent );
+
+		this.parent = parent;
+		this.config = config;
+		
 		// image copy
-		this.image = new Image( shell.getDisplay(), lcdCanvas.getBounds() );
+		capture( lcdCanvas );
+
+		shell = new Shell( parent, SWT.DIALOG_TRIM );
+		shell.setLayout( new FormLayout() );
+		shell.setText( "Screen Shot" );
+		shell.setLocation( parent.getLocation().x + parent.getSize().x + 50, parent.getLocation().y );
+		shell.addListener( SWT.Close, new Listener() {
+			@Override
+			public void handleEvent( Event event ) {
+				image.dispose();
+			}
+		} );
+		
+		Menu menu = new Menu( shell, SWT.BAR );
+		makeToolBarMenu( menu );
+		shell.setMenuBar( menu );
+
+		// canvas
+		ScrolledComposite composite = new ScrolledComposite( shell, SWT.V_SCROLL | SWT.H_SCROLL );
+		
+		canvas = new Canvas( composite, SWT.NONE );
+		composite.setContent( canvas );
+		
+		ImageData imageData = image.getImageData();
+		Rectangle rect = new Rectangle( 0, 0, imageData.width, imageData.height );
+		canvas.setBounds( rect );
+		
+		canvas.addPaintListener( new PaintListener() {
+			@Override
+			public void paintControl( PaintEvent e ) {
+				e.gc.drawImage( image, 0, 0 );
+			}
+		} );
+
+		int width = 0;
+		int height = 0;
+
+		Point shellLocation = shell.getLocation();
+		Rectangle monitorSize = shell.getDisplay().getClientArea();
+
+		if( shellLocation.y + imageData.height > monitorSize.height ) {
+			shell.setSize( imageData.width, imageData.height );
+		}else {
+			shell.pack();
+		}
+		
+	}
+
+	private void capture( Canvas canvas ) {
+		
+		if( null != this.image ) {
+			this.image.dispose();
+		}
+		
+		this.image = new Image( parent.getDisplay(), canvas.getBounds() );
+		
 		GC gc = null;
 		try {
-			gc = new GC( lcdCanvas );
-			gc.copyArea(image, 0, 0);
+			gc = new GC( canvas );
+			gc.copyArea( image, 0, 0 );
 		} finally {
 			if ( null != gc ) {
 				gc.dispose();
 			}
 		}
-
-		dialog = new Shell( shell, SWT.DIALOG_TRIM );
-		dialog.setLayout(new FormLayout());
-		dialog.setText( "Screen Shot" );
-		dialog.addListener( SWT.Close, new Listener() {
-			@Override
-			public void handleEvent( Event event ) {
-					image.dispose();
-				}
-		});
-
-		// tool bar
-		FormData barData = new FormData();
-		barData.left  = new FormAttachment( 0 );
-		barData.right = new FormAttachment( 100 );
-		barData.top   = new FormAttachment( 0 );
-		bar = new ToolBar( dialog, SWT.HORIZONTAL );
-		bar.setLayoutData( barData );
-		makeToolBarMenu();
-
-		// dummy for bar.getSize() 
-		dialog.setSize( lcdCanvas.getSize().x, lcdCanvas.getSize().y + bar.getSize().y + 1 );
-
-		dialog.setSize( lcdCanvas.getSize().x, lcdCanvas.getSize().y + bar.getSize().y + 29 /* Title bar */ );
-		dialog.update();
-
-		// canvas
-		ScrolledComposite composite = new ScrolledComposite( dialog, SWT.V_SCROLL | SWT.H_SCROLL );
-		canvas = new Canvas( composite, SWT.NONE );
-		composite.setContent( canvas );
-		Rectangle rect = image.getBounds();
-		rect = new Rectangle( rect.x, rect.y, rect.width, rect.height + bar.getSize().y );
-		canvas.setBounds(rect);
-		canvas.addPaintListener(  new PaintListener() {
-			@Override
-			public void paintControl( PaintEvent e ) {
-				e.gc.drawImage(image, 0, bar.getSize().y);
-			}
-		});
+		
+		canvas.update();
+		
 	}
-
-	private ToolItem saveToolItem;
-	private void makeToolBarMenu() {
-		saveToolItem = new ToolItem( bar, SWT.PUSH );
-		saveToolItem.setText( "Save" );
-		saveToolItem.setToolTipText( "Save image file" );
-		saveToolItem.addSelectionListener( new SelectionListener() {
+	
+	private void makeToolBarMenu( Menu menu ) {
+		
+		MenuItem saveItem = new MenuItem( menu, SWT.FLAT );
+		saveItem.setText( "Save" );
+		
+		saveItem.addSelectionListener( new SelectionAdapter() {
+			
 			@Override
 			public void widgetSelected( SelectionEvent e ) {
-				FileDialog fd = new FileDialog( dialog, SWT.SAVE );
+				
+				FileDialog fd = new FileDialog( shell, SWT.SAVE );
 				fd.setText( "Save Image... " );
 				String[] filter = { "*.png;*.PNG;*.jpg;*.JPG;*.jpeg;*.JPEG;*.bmp;*.BMP" };
 				String[] filterName = { "Image Files" };
@@ -136,55 +172,83 @@ public class ScreenShotDialog {
 				fd.setFilterNames( filterName );
 
 				SimpleDateFormat formatter = new SimpleDateFormat( "yyyy-mm-dd-hhmmss" );
-				fd.setFileName( "default-" + formatter.format( new Date(System.currentTimeMillis() ) ) + ".png" );
+				
+				String vmName = SkinUtil.getVmName( config );
+				
+				fd.setFileName( vmName + "-" + formatter.format( new Date( System.currentTimeMillis() ) ) + ".png" );
 
 				String path = fd.open();
-				if ( path == null ) {
+				if ( null == path ) {
 					return;
 				}
 
 				int i = path.lastIndexOf( '.' );
 				String filePath = path;
-				String format = null;
-				if (i != -1) {
-					filePath = path.substring( 0, i-1 );
-					format = path.substring(i+1, path.length());
+				String format = "";
+				
+				if ( -1 != i ) {
+					filePath = path.substring( 0, i - 1 );
+					format = path.substring( i + 1, path.length() );
 					format = format.toLowerCase();
 				}
 
 				FileOutputStream fos = null;
+				
 				try {
-					fos = new FileOutputStream( filePath + "." + format, false );
+					
+					fos = new FileOutputStream( filePath + "." + format, true );
+					
 					ImageLoader loader = new ImageLoader();
 					loader.data = new ImageData[] { image.getImageData() };
-					if (format == null || format.equals( "png" )) {
+					
+					if ( null == format || format.equals( "png" ) ) {
 						loader.save( fos, SWT.IMAGE_PNG );
 					} else if ( format.equals( "jpg" ) || format.equals( "jpeg" ) ) {
 						loader.save( fos, SWT.IMAGE_JPEG );
-					} else if (format.equals( "bmp" )) {
+					} else if ( format.equals( "bmp" ) ) {
 						loader.save( fos, SWT.IMAGE_BMP );
-					}  else {
-						MessageBox msg = new MessageBox( dialog,  SWT.ICON_WARNING );
+					} else {
+						
+						MessageBox msg = new MessageBox( shell, SWT.ICON_WARNING );
 						msg.setText( "Warning" );
-						msg.setMessage( "You must add the extension of file. (PNG/JPG/BMP)" );
+						msg.setMessage( "You have to use specific image formats. (PNG/JPG/BMP)" );
 						msg.open();
+						
 					}
-				} catch ( IOException ex )  {
-					ex.printStackTrace();
+					
+				} catch ( IOException ex ) {
+					logger.log( Level.SEVERE, ex.getMessage(), ex );
 				} finally {
 					IOUtil.close( fos );
 				}
 			}
-			@Override
-			public void widgetDefaultSelected( SelectionEvent e ) {
-			}
+
 		} );
+
+		
+		MenuItem refreshItem = new MenuItem( menu, SWT.FLAT );
+		refreshItem.setText( "Refresh" );
+		
+		saveItem.addSelectionListener( new SelectionAdapter() {
+			
+			@Override
+			public void widgetSelected( SelectionEvent e ) {
+				capture( canvas );
+			}
+
+		} );
+		
 	}
 
-	private static int count = 0;
 	public void open() {
-		dialog.setLocation( shell.getLocation().x + dialog.getSize().x + ( ++count % 5 ) * 20,
-							shell.getLocation().y + ( count % 5 ) * 20 );
-		dialog.open();
+
+		shell.open();
+
+		while ( !shell.isDisposed() ) {
+			if ( !shell.getDisplay().readAndDispatch() ) {
+				shell.getDisplay().sleep();
+			}
+		}
+
 	}
 }
