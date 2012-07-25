24497e44a4da33677288b6a0569c00783c5dafb0
syeon.hwang <syeon.hwang@samsung.com>
2012-07-25 16:06:05 +0900

    [Title] Add JNI compilation roution to ant script [Type] [Module] [Priority] [CQ#] [Redmine#] [Problem] [Cause] [Solution] [TestCase]

--- tizen/src/skin/client/native_src/make_so_linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-javah -classpath ../bin/:../lib/swt/gtk-linux/swt.jar -jni org.tizen.emulator.skin.EmulatorSkin
-rm ./org_tizen_emulator_skin_EmulatorSkin_PollFBThread.h
-rm ./org_tizen_emulator_skin_EmulatorSkin_SkinReopenPolicy.h
-gcc -c Share.c -o Share.o -I /usr/lib/jvm/jdk1.7.0_04/include -I /usr/lib/jvm/jdk1.7.0_04/include/linux
-gcc -shared Share.o -o libshared.so -fPIC
--- tizen/src/skin/client/native_src/make_so_mac.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-javah -classpath ../emulator-skin.jar:../lib/swt/cocoa-macosx64/swt.jar -jni org.tizen.emulator.skin.EmulatorSkin
-rm ./org_tizen_emulator_skin_EmulatorSkin_PollFBThread.h
-rm ./org_tizen_emulator_skin_EmulatorSkin_SkinReopenPolicy.h
-gcc -c Share.c -o Share.o -I/System/Library/Frameworks/JavaVM.framework/Headers
-gcc -dynamiclib Share.o -o libshared.dylib
