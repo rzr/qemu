4e562c07d07b9e49815be094271f79a2aa7a486f
jinhyung.jo <jinhyung.jo@samsung.com>
2012-09-03 13:29:14 +0900

    [Title] Performance downs due to the brightness. [Type] Bugfix [Module] Emulator [Priority] Major [CQ#] [Redmine#] [Problem] [Cause] [Solution] [TestCase]

--- tizen/src/hw/maru_brightness.c
+++ tizen/src/hw/maru_brightness.c
@@ -64,10 +64,11 @@ enum {
 uint32_t brightness_level = 24;
 uint32_t brightness_off = 0;
 
-/* level : 1 ~ 24, interval : 6 or 7. */
+/* level : 1 ~ 23, interval : 6 or 7. */
+/* skip 24 level, set to default alpha */
 uint8_t brightness_tbl[] = {97, /* level 0 : reserved for dimming */
-/* level 1 ~ 12 */          104, 110, 116, 122, 128, 134, 140, 146, 152, 158, 164, 170,
-/* level 13 ~ 24 */         176, 182, 188, 194, 200, 206, 213, 220, 227, 234, 241, 248};
+/* level 1 ~ 11 */          110, 116, 122, 128, 134, 140, 146, 152, 158, 164, 170,
+/* level 12 ~ 23 */         176, 182, 188, 194, 200, 206, 213, 220, 227, 234, 241, 248};
 
 static uint64_t brightness_reg_read( void *opaque, target_phys_addr_t addr, unsigned size ) {
     switch ( addr & 0xFF ) {
