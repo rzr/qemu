f8fd3988a463f9aef1232c2e0c2fb614a5ecd3ba
giwoong.kim <giwoong.kim@samsung.com>
2012-09-17 17:26:56 +0900

    [Title] modified swt library path in jni so script [Type] bugfix [Module] Emulator / mac [Priority] major [Jira#] [Redmine#] [Problem] [Cause] policy [Solution] [TestCase]

--- tizen/src/skin/client/native_src/make_so_linux.sh
+++ tizen/src/skin/client/native_src/make_so_linux.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-javah -classpath ../bin/:../lib/swt/gtk-linux/swt.jar -jni org.tizen.emulator.skin.EmulatorSkin
+javah -classpath ../bin/:../lib/swt.jar -jni org.tizen.emulator.skin.EmulatorSkin
 rm ./org_tizen_emulator_skin_EmulatorSkin_PollFBThread.h
 rm ./org_tizen_emulator_skin_EmulatorSkin_SkinReopenPolicy.h
+#TODO: jdk path
 gcc -c Share.c -o Share.o -I /usr/lib/jvm/jdk1.7.0_04/include -I /usr/lib/jvm/jdk1.7.0_04/include/linux
 gcc -shared Share.o -o libshared.so -fPIC
--- tizen/src/skin/client/native_src/make_so_mac.sh
+++ tizen/src/skin/client/native_src/make_so_mac.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-javah -classpath ../emulator-skin.jar:../lib/swt/cocoa-macosx64/swt.jar -jni org.tizen.emulator.skin.EmulatorSkin
+javah -classpath ../emulator-skin.jar:../lib/swt.jar -jni org.tizen.emulator.skin.EmulatorSkin
 rm ./org_tizen_emulator_skin_EmulatorSkin_PollFBThread.h
 rm ./org_tizen_emulator_skin_EmulatorSkin_SkinReopenPolicy.h
+#TODO: jdk path
 gcc -c Share.c -o Share.o -I/System/Library/Frameworks/JavaVM.framework/Headers
 gcc -dynamiclib Share.o -o libshared.dylib
