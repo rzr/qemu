rzr/qemu

commit 147ccdb569136af280c92595f4947256105e256b

build: fix build break on win32, macos

tizen/emulator_configure.sh
@@ -26,6 +26,7 @@ cd ..
 echo ""
 echo "##### QEMU configure for emulator"
 exec ./configure \
+ --target-list=i386-softmmu \
  --disable-werror \
  --audio-drv-list=alsa \
  --audio-card-list=ac97 \
@@ -46,6 +47,7 @@ cd ..
 echo ""
 echo "##### QEMU configure for emulator"
 exec ./configure \
+ --target-list=i386-softmmu \
  --audio-drv-list=winwave \
  --audio-card-list=ac97 \
  --enable-hax \
@@ -65,6 +67,7 @@ cd ..
 echo ""
 echo "##### QEMU configure for emulator"
 ./configure \
+ --target-list=i386-softmmu \
  --audio-drv-list=coreaudio \
  --enable-mixemu \
  --audio-card-list=ac97 \

Makefile.target
@@ -142,12 +142,11 @@ obj-$(CONFIG_XEN) += xen-all.o xen-mapcache.o
 obj-$(CONFIG_NO_XEN) += xen-stub.o
 
 # HAX support
-obj-$(CONFIG_HAX) += hax-all.o hax-windows.o
-obj-$(CONFIG_NO_HAX) += hax-stub.o
+ifdef CONFIG_WIN32
+obj-$(CONFIG_HAX) += target-i386/hax-all.o target-i386/hax-windows.o
+endif
 ifdef CONFIG_DARWIN
-obj-$(CONFIG_HAX) += \
-        hax-all.o       \
-        hax-darwin.o
+obj-$(CONFIG_HAX) += target-i386/hax-all.o target-i386/hax-darwin.o
 endif
 
 # Hardware support

tizen/src/sdb.h
@@ -112,4 +112,5 @@ void socket_close(int fd);
 void notify_sdb_daemon_start(void);
 int check_port_bind_listen(uint32_t port);
 int sdb_loopback_client(int port, int type);
-int check_port_bind_listen(u_int port);
+int check_port_bind_listen(uint32_t port);
+

commit 323c020d19ba80a3fb556af8b278f65409d07be9

tizen/emulator_configure.sh: Fix a bug introduced by 147ccdb569136af280c92595f4947256105e256b
The string "--target-list=i386-softmmu" shouldn't be there because appropriate target list is
generated inside build.sh script in $CONFIGURE_APPEND.

Signed-off-by: Evgeny Voevodin <e.voevodin@samsung.com>

tizen/emulator_configure.sh
@@ -26,7 +26,6 @@ cd ..
 echo ""
 echo "##### QEMU configure for emulator"
 exec ./configure \
- --target-list=i386-softmmu \
  --disable-werror \
  --audio-drv-list=alsa \
  --audio-card-list=ac97 \
@@ -47,7 +46,6 @@ cd ..
 echo ""
 echo "##### QEMU configure for emulator"
 exec ./configure \
- --target-list=i386-softmmu \
  --audio-drv-list=winwave \
  --audio-card-list=ac97 \
  --enable-hax \
@@ -67,7 +65,6 @@ cd ..
 echo ""
 echo "##### QEMU configure for emulator"
 ./configure \
- --target-list=i386-softmmu \
  --audio-drv-list=coreaudio \
  --enable-mixemu \
  --audio-card-list=ac97 \