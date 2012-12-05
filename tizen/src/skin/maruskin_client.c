c27065f045e5ae83f157050127130c655670420c
giwoong.kim <giwoong.kim@samsung.com>
2012-12-05 19:05:42 +0900

    skin: added double quotation mark for path
    Added double quotation mark for path of
    startup parameter.
    
    Signed-off-by: GiWoong Kim <giwoong.kim@samsung.com>

--- tizen/src/skin/maruskin_client.c
+++ tizen/src/skin/maruskin_client.c
@@ -115,13 +115,13 @@ static void* run_skin_client(void* arg)
         strlen(bin_dir) + strlen(JAR_SKINFILE) +
         strlen(OPT_SVR_PORT) + strlen(buf_skin_server_port) + strlen(OPT_UID) + strlen(buf_uid) +
         strlen(OPT_VM_PATH) + strlen(vm_path) + strlen(OPT_NET_BASE_PORT) + strlen(buf_tizen_base_port) +
-        strlen(OPT_MAX_TOUCHPOINT) + len_maxtouchpoint + strlen(argv) + 43;
+        strlen(OPT_MAX_TOUCHPOINT) + len_maxtouchpoint + strlen(argv) + 45;
     if (len > JAVA_MAX_COMMAND_LENGTH) {
         INFO("swt command length is too long! (%d)\n", len);
         len = JAVA_MAX_COMMAND_LENGTH;
     }
 
-    snprintf(cmd, len, "%s %s %s=. %s%s %s=\"%d\" %s=\"%d\" %s=\"%s\" %s=\"%d\" %s=%d %s",
+    snprintf(cmd, len, "%s %s %s=. \"%s%s\" %s=\"%d\" %s=\"%d\" %s=\"%s\" %s=\"%d\" %s=%d %s",
         JAVA_EXEFILE_PATH, JAVA_EXEOPTION, JAVA_LIBRARY_PATH,
         bin_dir, JAR_SKINFILE,
         OPT_SVR_PORT, skin_server_port,
