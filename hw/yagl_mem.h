c57550855d0766151606a251e3c37530a0f34a2d
Igor Mitsyanko <i.mitsyanko@samsung.com>
2013-01-11 20:13:32 +0400

    YaGL: convert memory accessing macros to inline functions for typesafety
    Convrt memory accessing macros to inline functions so we will get a compile time
    warning if we missuse any off these macros.
    
    Signed-off-by: Igor Mitsyanko <i.mitsyanko@samsung.com>

--- hw/yagl_mem.h
+++ hw/yagl_mem.h
@@ -29,24 +29,74 @@ bool yagl_mem_get_array(target_ulong va,
 char *yagl_mem_get_string(target_ulong va);
 
 #define yagl_mem_prepare_char(mt, va) yagl_mem_prepare_uint8((mt), (va))
-#define yagl_mem_put_char(mt, value) yagl_mem_put_uint8((mt), (uint8_t)(value))
-#define yagl_mem_get_char(va, value) yagl_mem_get_uint8((va), (uint8_t*)(value))
 #define yagl_mem_prepare_int8(mt, va) yagl_mem_prepare_uint8((mt), (va))
-#define yagl_mem_put_int8(mt, value) yagl_mem_put_uint8((mt), (uint8_t)(value))
-#define yagl_mem_get_int8(va, value) yagl_mem_get_uint8((va), (uint8_t*)(value))
 #define yagl_mem_prepare_int16(mt, va) yagl_mem_prepare_uint16((mt), (va))
-#define yagl_mem_put_int16(mt, value) yagl_mem_put_uint16((mt), (uint16_t)(value))
-#define yagl_mem_get_int16(va, value) yagl_mem_get_uint16((va), (uint16_t*)(value))
 #define yagl_mem_prepare_int32(mt, va) yagl_mem_prepare_uint32((mt), (va))
-#define yagl_mem_put_int32(mt, value) yagl_mem_put_uint32((mt), (uint32_t)(value))
-#define yagl_mem_get_int32(va, value) yagl_mem_get_uint32((va), (uint32_t*)(value))
 #define yagl_mem_prepare_host_handle(mt, va) yagl_mem_prepare_uint32((mt), (va))
-#define yagl_mem_put_host_handle(mt, value) yagl_mem_put_uint32((mt), (value))
-#define yagl_mem_get_host_handle(va, value) yagl_mem_get_uint32((va), (value))
+
+static inline void yagl_mem_put_char(struct yagl_mem_transfer *mt, char value)
+{
+    yagl_mem_put_uint8(mt, value);
+}
+
+static inline void yagl_mem_put_int8(struct yagl_mem_transfer *mt, int8_t value)
+{
+    yagl_mem_put_uint8(mt, value);
+}
+
+static inline void yagl_mem_put_int16(struct yagl_mem_transfer *mt,
+                                      int16_t value)
+{
+    yagl_mem_put_uint16(mt, value);
+}
+
+static inline void yagl_mem_put_int32(struct yagl_mem_transfer *mt,
+                                      int32_t value)
+{
+    yagl_mem_put_uint32(mt, value);
+}
+
+static inline void yagl_mem_put_host_handle(struct yagl_mem_transfer *mt,
+                                            yagl_host_handle value)
+{
+    yagl_mem_put_uint32(mt, value);
+}
+
+static inline bool yagl_mem_get_char(target_ulong va, char *value)
+{
+    return yagl_mem_get_uint8(va, (uint8_t *)value);
+}
+
+static inline bool yagl_mem_get_int8(target_ulong va, int8_t *value)
+{
+    return yagl_mem_get_uint8(va, (uint8_t *)value);
+}
+
+static inline bool yagl_mem_get_int16(target_ulong va, int16_t *value)
+{
+    return yagl_mem_get_uint16(va, (uint16_t *)value);
+}
+
+static inline bool yagl_mem_get_int32(target_ulong va, int32_t *value)
+{
+    return yagl_mem_get_uint32(va, (uint32_t *)value);
+}
+
+static inline bool yagl_mem_get_host_handle(target_ulong va,
+                                            yagl_host_handle *value)
+{
+    return yagl_mem_get_uint32(va, value);
+}
 
 #if TARGET_LONG_SIZE == 4
 #define yagl_mem_prepare_ptr(mt, va) yagl_mem_prepare_uint32((mt), (va))
-#define yagl_mem_put_ptr(mt, value) yagl_mem_put_uint32((mt), (value))
+
+static inline void yagl_mem_put_ptr(struct yagl_mem_transfer *mt,
+                                    target_ulong value)
+{
+    yagl_mem_put_uint32(mt, value);
+}
+
 #else
 #error 64-bit ptr not supported
 #endif
