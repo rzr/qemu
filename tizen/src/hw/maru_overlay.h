2557dd8f6401d60dd4e9faf8d5a83751384e8130
jinhyung.jo <jinhyung.jo@samsung.com>
2013-01-30 19:11:11 +0900

    maru_overlay : source clean up
    Corrected the boilerplates.
    Modified to suit the QEMU coding conventions.
    
    Signed-off-by: Jinhyung Jo <jinhyung.jo@samsung.com>

--- tizen/src/hw/maru_overlay.h
+++ tizen/src/hw/maru_overlay.h
@@ -1,17 +1,10 @@
 /*
  * Maru overlay device for VGA
  *
- * Copyright (C) 2011 - 2012 Samsung Electronics Co., Ltd. All rights reserved.
+ * Copyright (C) 2011 - 2013 Samsung Electronics Co., Ltd. All rights reserved.
  *
  * Contact:
- * SeokYeon Hwang <syeon.hwang@samsung.com>
- * SangJin Kim <sangjin3.kim@samsung.com>
- * MunKyu Im <munkyu.im@samsung.com>
- * KiTae Kim <kt920.kim@samsung.com>
  * JinHyung Jo <jinhyung.jo@samsung.com>
- * SungMin Ha <sungmin82.ha@samsung.com>
- * JiHye Kim <jihye1128.kim@samsung.com>
- * GiWoong Kim <giwoong.kim@samsung.com>
  * YeongKyoon Lee <yeongkyoon.lee@samsung.com>
  * DoHyung Hong
  * Hyunjun Son
@@ -28,7 +21,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
  *
  * Contributors:
  * - S-Core Co., Ltd
@@ -41,7 +35,7 @@
 
 #include "qemu-common.h"
 
-extern uint8_t* overlay_ptr;
+extern uint8_t *overlay_ptr;
 extern uint8_t overlay0_power;
 extern uint16_t overlay0_left;
 extern uint16_t overlay0_top;
@@ -54,6 +48,6 @@ extern uint16_t overlay1_top;
 extern uint16_t overlay1_width;
 extern uint16_t overlay1_height;
 
-DeviceState *pci_maru_overlay_init( PCIBus *bus );
+DeviceState *pci_maru_overlay_init(PCIBus *bus);
 
 #endif /* MARU_OVERLAY_H_ */
