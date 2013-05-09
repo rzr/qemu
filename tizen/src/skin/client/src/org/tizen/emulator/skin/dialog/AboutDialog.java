6c5fcb72110aeea862f412c9854f5c7e8c09012e
giwoong.kim <giwoong.kim@samsung.com>
2013-05-09 11:12:47 +0900

    menu: deleted a license button at About dialog
    deleted a license button at About dialog
    
    Change-Id: I863c78b7d4707284423c43e7d7a8bd0198933605
    Signed-off-by: GiWoong Kim <giwoong.kim@samsung.com>

--- tizen/src/skin/client/src/org/tizen/emulator/skin/dialog/AboutDialog.java
+++ tizen/src/skin/client/src/org/tizen/emulator/skin/dialog/AboutDialog.java
@@ -1,7 +1,7 @@
 /**
  * 
  *
- * Copyright (C) 2011 - 2012 Samsung Electronics Co., Ltd. All rights reserved.
+ * Copyright (C) 2011 - 2013 Samsung Electronics Co., Ltd. All rights reserved.
  *
  * Contact:
  * GiWoong Kim <giwoong.kim@samsung.com>
@@ -250,6 +250,7 @@ public class AboutDialog extends SkinDialog {
 		FillLayout fillLayout = new FillLayout(SWT.HORIZONTAL);
 		composite.setLayout(fillLayout);
 
+		/*
 		Button licenseButton = createButton(composite, "License");
 
 		licenseButton.addSelectionListener(new SelectionAdapter() {
@@ -265,6 +266,7 @@ public class AboutDialog extends SkinDialog {
 				}
 			}
 		});
+		*/
 
 		createOKButton(composite, true);
 	}
