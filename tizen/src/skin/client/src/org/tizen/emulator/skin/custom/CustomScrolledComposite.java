5ad60e9da984bc2d9f7175b4401268c666b75ba9
giwoong.kim <giwoong.kim@samsung.com>
2013-06-27 12:43:07 +0900

    skin: Set the background color of the shaft
    Set the background color of the shaft in CustomScrollBar
    
    Change-Id: I0e4c14d7bb8eeb24d287d42af73984adab6b4854
    Signed-off-by: GiWoong Kim <giwoong.kim@samsung.com>

--- tizen/src/skin/client/src/org/tizen/emulator/skin/custom/CustomScrolledComposite.java
+++ tizen/src/skin/client/src/org/tizen/emulator/skin/custom/CustomScrolledComposite.java
@@ -10,7 +10,7 @@
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
- * of the License, or ( at your option ) any later version.
+ * of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -64,8 +64,9 @@ public class CustomScrolledComposite extends Composite {
 	public boolean expandHorizontal = false;
 	public boolean expandVertical = false;
 
-	private int value;
-
+	/**
+	 *  Constructor
+	 */
 	public CustomScrolledComposite(Composite parent, int style,
 			Image[] imagesArrowUp, Image[] imagesArrowDown,
 			Image imageThumb, Image imageShaft) {
@@ -86,8 +87,6 @@ public class CustomScrolledComposite extends Composite {
 		this.imagesArrowDown = imagesArrowDown;
 		this.imageThumb = imageThumb;
 		this.imageShaft = imageShaft;
-
-		value = 0;
 	}
 
 	public CustomScrollBar getScrollBar() {
