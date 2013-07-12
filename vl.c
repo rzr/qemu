d0a2c0d49c260b69954187939ef64a9b897dae5b
SeokYeon Hwang <syeon.hwang@samsung.com>
2013-07-12 18:12:36 +0900

    Change memory allocation api to "g_malloc".
    A "0" initialize is not necessary to this memory allocation.
    
    Signed-off-by: SeokYeon Hwang <syeon.hwang@samsung.com>

--- vl.c
+++ vl.c
@@ -4231,7 +4231,7 @@ int main(int argc, char **argv, char **envp)
     }
 #ifdef CONFIG_MARU
     // W/A for preserve larger continuous heap for RAM.
-    preallocated_ptr = g_malloc0(ram_size);
+    preallocated_ptr = g_malloc(ram_size);
 #endif
 
     hax_pre_init(ram_size);
