76e3fa57d40269044d95ad2555cfd19f0ca06edd
Patrick Porlan <patrick.porlan@intel.com>
2013-09-23 15:34:35 +0200

    Correct formatting on Mac YaGL and Vigs modules
    indent -linux -i4 -nut -ts4

--- hw/vigs_gl_backend_agl.c
+++ hw/vigs_gl_backend_agl.c
@@ -5,7 +5,6 @@
 #include <glib.h>
 #include <dlfcn.h>
 
-
 #define LIBGL_IMAGE_NAME \
 "/System/Library/Frameworks/OpenGL.framework/Libraries/libGL.dylib"
 
@@ -18,18 +17,17 @@
         } \
     } while (0)
 
-struct vigs_gl_backend_agl
-{
+struct vigs_gl_backend_agl {
     struct vigs_gl_backend base;
-    
+
     void *handle;
     AGLContext context;
     AGLPbuffer surface;
     AGLPixelFormat pixfmt;
 };
 
-
-static int vigs_gl_backend_agl_choose_config(struct vigs_gl_backend_agl *gl_backend_agl)
+static int vigs_gl_backend_agl_choose_config(struct vigs_gl_backend_agl
+                                             *gl_backend_agl)
 {
     const int attrib_list[] = {
         AGL_RGBA,
@@ -48,99 +46,97 @@ static int vigs_gl_backend_agl_choose_config(struct vigs_gl_backend_agl *gl_back
         AGL_NONE
     };
     AGLPixelFormat pixfmt;
-  
+
     /* Select first AGL pixel format matching our constraints */
-    
+
     pixfmt = aglChoosePixelFormat(NULL, 0, attrib_list);
-    
+
     gl_backend_agl->pixfmt = pixfmt;
-    
+
     return (pixfmt != NULL);
 }
 
-
-static bool vigs_gl_backend_agl_create_surface(struct vigs_gl_backend_agl *gl_backend_agl,
-                                               int config_id)
+static bool vigs_gl_backend_agl_create_surface(struct vigs_gl_backend_agl
+                                               *gl_backend_agl, int config_id)
 {
     gl_backend_agl->surface = NULL;
-    
-    aglCreatePBuffer(2048, 2048, GL_TEXTURE_2D, GL_RGBA, 0, &gl_backend_agl->surface);
-    
+
+    aglCreatePBuffer(2048, 2048, GL_TEXTURE_2D, GL_RGBA, 0,
+                     &gl_backend_agl->surface);
+
     if (!gl_backend_agl->surface) {
         VIGS_LOG_CRITICAL("aglCreatePBuffer failed");
         return false;
     }
-    
+
     return true;
 }
 
-
-static bool vigs_gl_backend_agl_create_context(struct vigs_gl_backend_agl *gl_backend_agl)
+static bool vigs_gl_backend_agl_create_context(struct vigs_gl_backend_agl
+                                               *gl_backend_agl)
 {
     gl_backend_agl->context = aglCreateContext(gl_backend_agl->pixfmt, NULL);
-    
+
     if (!gl_backend_agl->context) {
         VIGS_LOG_CRITICAL("aglCreateContext failed");
         return false;
     }
-    
+
     return true;
 }
 
-
 static void vigs_gl_backend_agl_destroy(struct vigs_backend *backend)
 {
-    struct vigs_gl_backend_agl *gl_backend_agl = (struct vigs_gl_backend_agl*)backend;
-    
+    struct vigs_gl_backend_agl *gl_backend_agl =
+        (struct vigs_gl_backend_agl *)backend;
+
     vigs_gl_backend_cleanup(&gl_backend_agl->base);
-    
+
     if (gl_backend_agl->surface) {
         aglDestroyPBuffer(gl_backend_agl->surface);
     }
-    
+
     if (gl_backend_agl->context) {
         aglDestroyContext(gl_backend_agl->context);
     }
-    
+
     if (gl_backend_agl->handle) {
         dlclose(gl_backend_agl->handle);
     }
-    
+
     vigs_backend_cleanup(&gl_backend_agl->base.base);
-    
+
     g_free(gl_backend_agl);
-    
+
     VIGS_LOG_DEBUG("destroyed");
 }
-
 
 static bool vigs_gl_backend_agl_has_current(struct vigs_gl_backend *gl_backend)
 {
     return aglGetCurrentContext() != NULL;
 }
 
-
 static bool vigs_gl_backend_agl_make_current(struct vigs_gl_backend *gl_backend,
                                              bool enable)
 {
     struct vigs_gl_backend_agl *gl_backend_agl =
-    (struct vigs_gl_backend_agl*)gl_backend;
+        (struct vigs_gl_backend_agl *)gl_backend;
     AGLPbuffer buf = NULL;
     AGLContext context = gl_backend_agl->context;
-    
+
     if (enable) {
         buf = gl_backend_agl->surface;
-        
+
         if (!buf) {
             VIGS_LOG_CRITICAL("surface retrieval failed");
             return false;
         }
-        
+
         if (aglSetPBuffer(context, buf, 0, 0, 0) == GL_FALSE) {
             VIGS_LOG_CRITICAL("aglSetPBuffer failed");
             return false;
         }
-            
+
         if (aglSetCurrentContext(context) == GL_FALSE) {
             VIGS_LOG_CRITICAL("aglSetCurrentContext failed");
             aglSetPBuffer(context, NULL, 0, 0, 0);
@@ -152,23 +148,22 @@ static bool vigs_gl_backend_agl_make_current(struct vigs_gl_backend *gl_backend,
             return false;
         }
     }
-    
+
     return true;
 }
-
 
 struct vigs_backend *vigs_gl_backend_create(void *display)
 {
     struct vigs_gl_backend_agl *gl_backend_agl;
     int config_id;
-    
+
     gl_backend_agl = g_malloc0(sizeof(*gl_backend_agl));
-    
+
     vigs_backend_init(&gl_backend_agl->base.base,
                       &gl_backend_agl->base.ws_info.base);
-    
+
     gl_backend_agl->handle = dlopen(LIBGL_IMAGE_NAME, RTLD_GLOBAL);
-    
+
     VIGS_GL_GET_PROC(GenTextures, glGenTextures);
     VIGS_GL_GET_PROC(DeleteTextures, glDeleteTextures);
     VIGS_GL_GET_PROC(BindTexture, glBindTexture);
@@ -221,52 +216,51 @@ struct vigs_backend *vigs_gl_backend_create(void *display)
     VIGS_GL_GET_PROC(BlendFunc, glBlendFunc);
     VIGS_GL_GET_PROC(CopyTexImage2D, glCopyTexImage2D);
     VIGS_GL_GET_PROC(BlitFramebuffer, glBlitFramebufferEXT);
-    
+
     config_id = vigs_gl_backend_agl_choose_config(gl_backend_agl);
-    
+
     if (!config_id) {
         goto fail;
     }
-    
+
     if (!vigs_gl_backend_agl_create_surface(gl_backend_agl, config_id)) {
         goto fail;
     }
-    
+
     if (!vigs_gl_backend_agl_create_context(gl_backend_agl)) {
         goto fail;
     }
-    
-    
+
     gl_backend_agl->base.base.destroy = &vigs_gl_backend_agl_destroy;
     gl_backend_agl->base.has_current = &vigs_gl_backend_agl_has_current;
     gl_backend_agl->base.make_current = &vigs_gl_backend_agl_make_current;
     gl_backend_agl->base.ws_info.context = &gl_backend_agl->context;
-    
+
     if (!vigs_gl_backend_init(&gl_backend_agl->base)) {
         goto fail;
     }
-    
+
     VIGS_LOG_DEBUG("created");
-    
+
     return &gl_backend_agl->base.base;
-    
-fail:
-    
+
+ fail:
+
     if (gl_backend_agl->handle) {
         dlclose(gl_backend_agl->handle);
     }
-    
+
     if (gl_backend_agl->surface) {
         aglDestroyPBuffer(gl_backend_agl->surface);
     }
-    
+
     if (gl_backend_agl->context) {
-        aglDestroyContext(gl_backend_agl->context);  
+        aglDestroyContext(gl_backend_agl->context);
     }
-    
+
     vigs_backend_cleanup(&gl_backend_agl->base.base);
-    
+
     g_free(gl_backend_agl);
-    
+
     return NULL;
 }
