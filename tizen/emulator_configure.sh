fa3b1b759a63b04265706f29306d8bd1b78c4dda
Kitae Kim <kt920.kim@samsung.com>
2014-03-19 16:38:53 +0900

    libav: remove libav from qemu
    As separate libav from qemu, it is easy to maintain qemu source.
    In addition to this, as libav will be linked dynamically to qemu,
    the size of qemu binary will be reduced.
    
    Change-Id: I5d2a7d3657abf9797a47a1055afc39fe1bfeb56b
    Signed-off-by: Kitae Kim <kt920.kim@samsung.com>

--- tizen/emulator_configure.sh
+++ tizen/emulator_configure.sh
@@ -137,14 +137,6 @@ set_target() {
 
 # OS specific
 targetos=`uname -s`
-targetarch=`echo | gcc -E -dM - | grep __x86_64`
-bindir="i386"
-ffmpegarc="x86"
-if test "$targetarch" != ""
-then
-    bindir="x86_64"
-    ffmpegarc="x86_64"
-fi
 echo "##### checking for os... targetos $targetos"
 
 echo "$*"
@@ -219,15 +211,7 @@ fi
 
 case $targetos in
 Linux*)
-cd distrib/libav
-echo ""
-echo "##### FFMPEG configure for emulator"
-./configure \
- --prefix=./$bindir --arch=${ffmpegarc} --enable-static --enable-pic --enable-optimizations --disable-doc --disable-gpl --disable-yasm --disable-postproc --disable-swscale --disable-ffmpeg --disable-ffprobe --disable-ffserver --disable-ffplay --disable-decoders --disable-encoders --disable-muxers --disable-demuxers --disable-parsers --disable-protocols --disable-network --disable-bsfs --disable-devices --disable-filters --enable-encoder=aac --enable-encoder=h263 --enable-encoder=h263p --enable-encoder=mpeg4 --enable-encoder=msmpeg4v2 --enable-encoder=msmpeg4v3 --enable-decoder=aac --enable-decoder=h263 --enable-decoder=h264 --enable-decoder=mp3 --enable-decoder=mp3adu --enable-decoder=mpeg4 --enable-decoder=mpegvideo --enable-decoder=msmpeg4v1 --enable-decoder=msmpeg4v2 --enable-decoder=msmpeg4v3 --enable-decoder=wmav1 --enable-decoder=wmav2 --enable-decoder=wmv3 --enable-decoder=vc1
-cd ../.. 
-
 cd ..
-
 echo ""
 echo "##### QEMU configuring for emulator"
 echo "##### QEMU configure append:" $CONFIGURE_APPEND
@@ -243,13 +227,6 @@ exec ./configure \
  $CONFIGURE_APPEND \
 ;;
 MINGW*)
-cd distrib/libav
-echo ""
-echo "##### FFMPEG configure for emulator"
-./configure \
- --prefix=./$bindir --arch=x86 --enable-static --enable-w32threads --enable-optimizations --enable-memalign-hack --disable-doc --disable-gpl --disable-yasm --disable-postproc --disable-swscale --disable-ffmpeg --disable-ffprobe --disable-ffserver --disable-ffplay --disable-decoders --disable-encoders --disable-muxers --disable-demuxers --disable-parsers --disable-protocols --disable-network --disable-bsfs --disable-devices --disable-filters --enable-encoder=aac --enable-encoder=h263 --enable-encoder=h263p --enable-encoder=mpeg4 --enable-encoder=msmpeg4v2 --enable-encoder=msmpeg4v3 --enable-decoder=aac --enable-decoder=h263 --enable-decoder=h264 --enable-decoder=mp3 --enable-decoder=mp3adu --enable-decoder=mpeg4 --enable-decoder=mpegvideo --enable-decoder=msmpeg4v1 --enable-decoder=msmpeg4v2 --enable-decoder=msmpeg4v3 --enable-decoder=wmav1 --enable-decoder=wmav2 --enable-decoder=wmv3 --enable-decoder=vc1
-cd ../..
-
 cd ..
 echo ""
 echo "##### QEMU configuring for emulator"
@@ -267,13 +244,6 @@ exec ./configure \
  $CONFIGURE_APPEND \
 ;;
 Darwin*)
-cd distrib/libav
-echo ""
-echo "##### FFMPEG configure for emulator"
-./configure \
---prefix=./$bindir --extra-cflags=-mmacosx-version-min=10.4 --arch=x86 --enable-static --enable-pic --enable-optimizations --disable-doc --disable-gpl --disable-yasm --disable-postproc --disable-swscale --disable-ffmpeg --disable-ffprobe --disable-ffserver --disable-ffplay --disable-decoders --disable-encoders --disable-muxers --disable-demuxers --disable-parsers --disable-protocols --disable-network --disable-bsfs --disable-devices --disable-filters --enable-encoder=aac --enable-encoder=h263 --enable-encoder=h263p --enable-encoder=mpeg4 --enable-encoder=msmpeg4v2 --enable-encoder=msmpeg4v3 --enable-decoder=aac --enable-decoder=h263 --enable-decoder=h264 --enable-decoder=mp3 --enable-decoder=mp3adu --enable-decoder=mpeg4 --enable-decoder=mpegvideo --enable-decoder=msmpeg4v1 --enable-decoder=msmpeg4v2 --enable-decoder=msmpeg4v3 --enable-decoder=wmav1 --enable-decoder=wmav2 --enable-decoder=wmv3 --enable-decoder=vc1 --cc=cc
-cd ../..
-
 cd ..
 echo ""
 echo "##### QEMU configuring for emulator"
