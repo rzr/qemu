23bfc6dfe5b3d2feedd689c91ed05eb84e29d46e
jinhyung.jo <jinhyung.jo@samsung.com>
2014-03-26 17:20:03 +0900

    VirtGL : Modified source files
    as the QEMU coding style
    
    Change-Id: If1134995efeaabc9502cfec5bd0f2005f6d43af1
    Signed-off-by: Jinhyung Jo <jinhyung.jo@samsung.com>

--- tizen/src/hw/virtio-gl.c
+++ tizen/src/hw/virtio-gl.c
@@ -24,222 +24,177 @@
  * THE SOFTWARE.
  */
 
-#if 0
-#include "hw.h"
-#include "maru_device_ids.h"
-
-typedef target_phys_addr_t arg_t;
-#include "opengl_process.h"
-#include "opengl_exec.h"
-#include "helper_opengl.h"
-#include <sys/time.h>
-
-#include "tizen/src/debug_ch.h"
-MULTI_DEBUG_CHANNEL(qemu, virtio-gl);
-
-#define TYPE_VIRTIO_GL	"virtio-gl"
-
-int decode_call_int(ProcessStruct *p, char *in_args, int args_len, char *r_buffer);
-
-/* Uncomment to enable debugging - WARNING!!! changes ABI! */
-//#define DEBUG_GLIO
-
-typedef struct VirtIOGL
-{
-	VirtIODevice vdev;
-	VirtQueue *vq;
-} VirtIOGL;
-
-struct d_hdr
-{
-	int pid;
-	int rq_l;
-	int rrq_l;
-#ifdef DEBUG_GLIO
-	int sum;
-#endif
-};
-
-#define SIZE_OUT_HEADER sizeof(struct d_hdr)
-
-#ifdef DEBUG_GLIO
-#define SIZE_IN_HEADER (4*2)
-#else
-#define SIZE_IN_HEADER 4
-#endif
-
-#endif
 #include "virtio-gl.h"
 
 static void virtio_gl_handle(VirtIODevice *vdev, VirtQueue *vq)
 {
-	VirtQueueElement elem;
-	int nkill = 1;
+    VirtQueueElement elem;
+    int nkill = 1;
 
-	TRACE("virtio_gl_handle called. \n");
+    TRACE("virtio_gl_handle called.\n");
 
-	while(virtqueue_pop(vq, &elem)) {
-		struct d_hdr *hdr = (struct d_hdr*)elem.out_sg[0].iov_base;
-		ProcessStruct *process;
-		int i, remain;
-		int ret = 0;
+    while (virtqueue_pop(vq, &elem)) {
+        struct d_hdr *hdr = (struct d_hdr *)elem.out_sg[0].iov_base;
+        ProcessStruct *process;
+        int i, remain;
+        int ret = 0;
 
-		if(!elem.out_num) {
-			fprintf(stderr, "Bad packet\n");
-			virtqueue_push(vq, &elem, ret);
-			virtio_notify(vdev, vq);
-			return;
-		}
+        if (!elem.out_num) {
+            ERR("Bad packet\n");
+            virtqueue_push(vq, &elem, ret);
+            virtio_notify(vdev, vq);
+            return;
+        }
 
-		process = vmgl_get_process(hdr->pid);
+        process = vmgl_get_process(hdr->pid);
 
-		if(hdr->rq_l) {
-
-			if(hdr->rrq_l) {
-				if(process->rq) {  // Usually only the quit packet...
-					free(process->rq);
-					free(process->rrq);
-				}
-				//                process->rq  = process->rq_p  = qemu_malloc(hdr->rq_l);
-				//                process->rrq = process->rrq_p = qemu_malloc(hdr->rrq_l);
-				process->rq  = process->rq_p  = g_malloc(hdr->rq_l);
-				process->rrq = process->rrq_p = g_malloc(hdr->rrq_l);
-				process->rq_l  = hdr->rq_l - SIZE_OUT_HEADER;
-				process->rrq_l = hdr->rrq_l;
+        if (hdr->rq_l) {
+            if (hdr->rrq_l) {
+                if (process->rq) { /* Usually only the quit packet... */
+                    free(process->rq);
+                    free(process->rrq);
+                }
+                process->rq  = process->rq_p  = g_malloc(hdr->rq_l);
+                process->rrq = process->rrq_p = g_malloc(hdr->rrq_l);
+                process->rq_l  = hdr->rq_l - SIZE_OUT_HEADER;
+                process->rrq_l = hdr->rrq_l;
 #ifdef DEBUG_GLIO
-				process->sum = hdr->sum;
+                process->sum = hdr->sum;
 #endif
-			}
+            }
 
-			i = 0;
-			remain = process->rq_l - (process->rq_p - process->rq);
-			while(remain && i < elem.out_num){
-				char *src = (char *)elem.out_sg[i].iov_base;
-				int ilen = elem.out_sg[i].iov_len;
-				int len = remain;
+            i = 0;
+            remain = process->rq_l - (process->rq_p - process->rq);
+            while (remain && i < elem.out_num) {
+                char *src = (char *)elem.out_sg[i].iov_base;
+                int ilen = elem.out_sg[i].iov_len;
+                int len = remain;
 
-				if(i == 0) {
-					src += SIZE_OUT_HEADER;
-					ilen -= SIZE_OUT_HEADER;
-				}
+                if (i == 0) {
+                    src += SIZE_OUT_HEADER;
+                    ilen -= SIZE_OUT_HEADER;
+                }
 
-				if(len > ilen)
-					len = ilen;
+                if (len > ilen) {
+                    len = ilen;
+                }
 
-				memcpy(process->rq_p, src, len);
-				process->rq_p += len;
-				remain -= len;
-				i++;
-			}
+                memcpy(process->rq_p, src, len);
+                process->rq_p += len;
+                remain -= len;
+                i++;
+            }
 
-			if(process->rq_p >= process->rq + process->rq_l) {
-
+            if (process->rq_p >= process->rq + process->rq_l) {
 #ifdef DEBUG_GLIO
-				int sum = 0;
-				for(i = 0; i < process->rq_l ; i++)
-					sum += process->rq[i];
-				if(sum != process->sum)
-					fprintf(stderr, "Checksum fail\n");
-#endif
-
-				*(int*)process->rrq = nkill = decode_call_int(process, 
-						process->rq,      /* command_buffer */
-						process->rq_l,    /* cmd buffer length */
-						process->rrq + SIZE_IN_HEADER);    /* return buffer */
-
-				//qemu_free(process->rq);
-				g_free(process->rq);
-				process->rq = NULL;
-
-#ifdef DEBUG_GLIO
-				sum = 0;
-				for(i = SIZE_IN_BUFFER; i < process->rrq_l ; i++)
-					sum += process->rrq[i];
-				*(int*)(process->rrq+sizeof(int)) = sum;
+                int sum = 0;
+                for (i = 0; i < process->rq_l; i++) {
+                    sum += process->rq[i];
+                }
+                if (sum != process->sum) {
+                    ERR("Checksum fail\n");
+                }
 #endif
 
-			}
-		}
+                *(int *)process->rrq = nkill = decode_call_int(process,
+                        process->rq,      /* command_buffer */
+                        process->rq_l,    /* cmd buffer length */
+                        process->rrq + SIZE_IN_HEADER);    /* return buffer */
 
-		if(hdr->rrq_l && process->rq == NULL) {
+                g_free(process->rq);
+                process->rq = NULL;
 
-			i = 0;
-			remain = process->rrq_l - (process->rrq_p - process->rrq);
-			while(remain && i < elem.in_num) {
-				char *dst = elem.in_sg[i].iov_base;
-				int len = remain;
-				int ilen = elem.in_sg[i].iov_len;
+#ifdef DEBUG_GLIO
+                sum = 0;
+                for (i = SIZE_IN_BUFFER; i < process->rrq_l; i++) {
+                    sum += process->rrq[i];
+                }
+                *(int *)(process->rrq + sizeof(int)) = sum;
+#endif
 
-				if(len > ilen)
-					len = ilen;
+            }
+        }
 
-				memcpy(dst, process->rrq_p, len);
-				process->rrq_p += len;
-				remain -= len;
-				ret += len;
-				i++;
-			}
+        if (hdr->rrq_l && process->rq == NULL) {
+            i = 0;
+            remain = process->rrq_l - (process->rrq_p - process->rrq);
+            while (remain && i < elem.in_num) {
+                char *dst = elem.in_sg[i].iov_base;
+                int len = remain;
+                int ilen = elem.in_sg[i].iov_len;
 
-			if(remain <= 0) {
-				//qemu_free(process->rrq);
-				g_free(process->rrq);
-				if(!nkill)
-					gl_disconnect(process);
-			}
-		}
+                if (len > ilen) {
+                    len = ilen;
+                }
 
-		virtqueue_push(vq, &elem, ret);
+                memcpy(dst, process->rrq_p, len);
+                process->rrq_p += len;
+                remain -= len;
+                ret += len;
+                i++;
+            }
 
-		virtio_notify(vdev, vq);
-	}
+            if (remain <= 0) {
+                g_free(process->rrq);
+                if (!nkill) {
+                    gl_disconnect(process);
+                }
+            }
+        }
+
+        virtqueue_push(vq, &elem, ret);
+        virtio_notify(vdev, vq);
+    }
 }
 
 static uint32_t virtio_gl_get_features(VirtIODevice *vdev, uint32_t f)
 {
-	return 0;
+    return 0;
 }
 
 /*
 static void virtio_gl_save(QEMUFile *f, void *opaque)
 {
-	VirtIOGL *s = opaque;
+    VirtIOGL *s = opaque;
 
-	virtio_save(&s->vdev, f);
+    virtio_save(&s->vdev, f);
 }
 
 static int virtio_gl_load(QEMUFile *f, void *opaque, int version_id)
 {
-	VirtIOGL *s = opaque;
+    VirtIOGL *s = opaque;
 
-	if (version_id != 1)
-		return -EINVAL;
+    if (version_id != 1) {
+        return -EINVAL;
+    }
 
-	virtio_load(&s->vdev, f);
-	return 0;
+    virtio_load(&s->vdev, f);
+    return 0;
 }
 */
 
 static int virtio_gl_device_init(VirtIODevice *vdev)
 {
-	VirtIOGL *s = VIRTIO_GL(vdev);
-	//DeviceState *qdev = DEVICE(vdev);
+    VirtIOGL *s = VIRTIO_GL(vdev);
+    /* DeviceState *qdev = DEVICE(vdev); */
+    if (s == NULL) {
+        ERR("Failed to initialize virtio gl device.\n");
+        return -1;
+    }
 
-	virtio_init(vdev, TYPE_VIRTIO_GL, VIRTIO_ID_GL, 0);
-	if (vdev == NULL) {
-		ERR("Failed to initialize virtio gl device.\n");
-		return -1;
-	}
+    virtio_init(vdev, TYPE_VIRTIO_GL, VIRTIO_ID_GL, 0);
 
-	/*
-	VirtioDeviceClass *vdc = VIRTIO_DEVICE_GET_CLASS(&s->vdev);
-	vdc->get_features = virtio_gl_get_features;
+    /*
+    VirtioDeviceClass *vdc = VIRTIO_DEVICE_GET_CLASS(&s->vdev);
+    vdc->get_features = virtio_gl_get_features;
 
-	s->vq = virtio_add_queue(&s->vdev, 128, virtio_gl_handle);
-	register_savevm(qdev, TYPE_VIRTIO_GL, -1, 1, virtio_gl_save, virtio_gl_load, s);
-	*/
-	s->vq = virtio_add_queue(vdev, 128, virtio_gl_handle);
+    s->vq = virtio_add_queue(&s->vdev, 128, virtio_gl_handle);
+    register_savevm(qdev, TYPE_VIRTIO_GL, -1, 1,
+                    virtio_gl_save, virtio_gl_load, s);
+    */
+    s->vq = virtio_add_queue(vdev, 128, virtio_gl_handle);
 
-	return 0;
+    return 0;
 }
 
 static int virtio_gl_device_exit(DeviceState *qdev)
