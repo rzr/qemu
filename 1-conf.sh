98c8c5d232d577f8bc3af29ceb432229a3611259
Stanislav Vorobiov <s.vorobiov@samsung.com>
2014-06-05 19:02:38 +0400

    add configure/run scripts and kernel

---
+++ 1-conf.sh
@@ -0,0 +1,1 @@
+./configure --target-list=i386-softmmu --enable-werror --audio-drv-list=alsa --disable-vnc --disable-pie --enable-sdl
---
+++ 1-run.sh
@@ -0,0 +1,13 @@
+./i386-softmmu/qemu-system-i386 \
+-drive file=./data/tizen-common.x86,if=virtio,index=1 \
+-boot c \
+-append 'console=ttyS0 root=/dev/vda rw video=LVDS-1:1080x1920-32@30 ip=10.0.2.15::10.0.2.2:255.255.255.0::eth0:none' \
+-serial file:./data/kernel.log \
+-m 512 \
+-M pc \
+-kernel ./data/bzImage \
+-enable-kvm \
+-smp `grep -c 'cpu cores' /proc/cpuinfo` \
+-vga none \
+-device vigs \
+-device yagl 1>./data/qemu.log 2>&1
