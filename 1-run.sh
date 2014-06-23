f33d87ba4192c0e734ec6f3439411a433ec29c6f
Stanislav Vorobiov <s.vorobiov@samsung.com>
2014-06-23 14:06:57 +0400

    kernel update
    SMACK issues fixed, security=none is not needed anymore

--- 1-run.sh
+++ 1-run.sh
@@ -1,7 +1,7 @@
 ./i386-softmmu/qemu-system-i386 \
 -drive file=./data/tizen-common.x86,if=virtio,index=1 \
 -boot c \
--append 'security=none console=ttyS0 root=/dev/vda rw video=LVDS-1:1920x1080-32@60 ip=10.0.2.15::10.0.2.2:255.255.255.0::eth0:none' \
+-append 'console=ttyS0 root=/dev/vda rw video=LVDS-1:1920x1080-32@60 ip=10.0.2.15::10.0.2.2:255.255.255.0::eth0:none' \
 -serial file:./data/kernel.log \
 -m 512 \
 -M pc \
