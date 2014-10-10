137cec47592ec5417b7903a1b4ac0d9266083dc2
Kitae Kim <kt920.kim@samsung.com>
2014-10-10 10:28:47 +0900

    build: remove gcc dependency on Mac OS X.
    Change-Id: I1ffa24aafe9fbdfddcb72e4d0ae4bf4e52ef2b87
    Signed-off-by: Kitae Kim <kt920.kim@samsung.com>

--- tizen/src/skin/client/native_src/make_so_mac.sh
+++ tizen/src/skin/client/native_src/make_so_mac.sh
@@ -5,5 +5,5 @@
 javah -classpath ../emulator-skin.jar:../lib/swt.jar -jni org.tizen.emulator.skin.EmulatorShmSkin
 rm ./org_tizen_emulator_skin_EmulatorShmSkin_PollFBThread.h
 #TODO: jdk path
-gcc -c share.c -o share.o -I/System/Library/Frameworks/JavaVM.framework/Headers
-gcc -dynamiclib share.o -o libshared.dylib
+cc -c share.c -o share.o -I/System/Library/Frameworks/JavaVM.framework/Headers
+cc -dynamiclib share.o -o libshared.dylib
